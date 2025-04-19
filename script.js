17c2c80a172ef92f7b13be278a157eb204a22995
JalixWebTech <jamefelix2003@gmail.com>
2025-04-19 05:05:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 
-  function sendToWhatsApp() {
-      let email = document.getElementById("email").value;
-      let whatsappNumber = document.getElementById("whatsappNumber").value;
-      let message = document.getElementById("message").value;
+  document.getElementById("whatsappForm").addEventListener("submit", function(e) {
+      e.preventDefault();
 
-      if (email === "" || whatsappNumber === "" || message === "") {
-          alert("Please fill in all fields.");
-          return;
-      }
-      let yourWhatsApp = "2349127463774";
-      window.open(url, "_blank");
-    }
+      var name = document.getElementById("name").value;
+     var name = document.getElementById("number").value;
+      var email = document.getElementById("email").value;
+      var message = document.getElementById("message").value;
+
+      var text = Hello, my name is ${name}. My email is ${email}. Message: ${message};
+      var phoneNumber = "2349127463774"; // Replace with your WhatsApp number (with country code)
+
+      var whatsappURL = https://wa.me/${phoneNumber}?text=${encodeURIComponent(text)};
+      window.open(whatsappURL, '_blank');
+    });
